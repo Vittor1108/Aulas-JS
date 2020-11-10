2a1db58ce14ab3573048f770f2f10cd545cdcddc
Vittor Daniel <vittordaniel1108@gmail.com>
2020-11-10 12:18:31 -0300

    commit full corse

---
+++ Arquivos das Aulas/Aulas/aula11/ex008.js
@@ -0,0 +1,5 @@
+var vel = 50;
+if(vel >= 80){
+    console.log(`Você ultrapassou o limiete de velocidade!MULTADO!`);
+}
+console.log(`Dirija sempre com segurança!`);
---
+++ Arquivos das Aulas/Aulas/aula12/ex012.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var hora = agora.getHours();
+console.log(`Agora são exatamente ${hora} horas!`);
+
+if(hora < 12){
+    console.log(`Bom Dia!`);
+}
+else if (hora <= 12){
+    console.log(`Boa tarde!`);
+}
+else if (hora > 19){
+    console.log(`Boa Noite!`);
+}    
