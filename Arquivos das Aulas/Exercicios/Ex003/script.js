1c3efd03a61c4e3f43c8722261d83de3b51059c6
Vittor Daniel <vittordaniel1108@gmail.com>
2020-11-17 16:14:42 -0300

    Correção exc 04

--- Arquivos das Aulas/Exercicios/Ex003/script.js
+++ Arquivos das Aulas/Exercicios/Ex003/script.js
@@ -1,12 +1,36 @@
 function contar(){
-    var inicio  = document.getElementById('n1');
-    var fim = document.getElementById('n2');
-    var passos = document.getEelement('n3'); 
-    var res = document.getElementById('container-2');
-
-    if(inicio.value.length == 0 || fim.value.length == 0 || passos.value.length == 0 ){
-        alert("[ERRO] Por favor verifique os dados e tente novamente!");
-    }else{
-        alert('tudo certo')
+    var inicio = document.querySelector('#n1');
+    var fim = document.querySelector('#n2');
+    var passos = document.querySelector('#passos');
+    var output = document.querySelector('#container-2');
+    
+  
+    if(inicio.length == 0 || fim.length == 0 || passos.length == 0){
+        output.innerHTML = `Impossível Contar`;
+        alert('[ERRO!!] Confira os dados e tente novamente.');
     }
+    else{
+        output.innerHTML = `Contando:`
+        var i = Number(inicio.value);
+        var f = Number(fim.value);
+        var p = Number(passos.value);
+        if(p <= 0){
+            alert("Passo invalido!");
+        }
+
+        if(i < f){
+            //contagem crescente
+            for(var c = i; c < f; c += p){
+                output.innerHTML += ` ${c} `;
+           }
+        }
+        //contagem regressiva
+        else{
+            for(var c = i; c >= f; c -= p){
+                output.innerHTML += ` ${c} ` ;
+            }
+        }
+       
+    }
+  
 }
